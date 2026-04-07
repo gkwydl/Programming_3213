c28f60dc2e2b332c9c9c0419277888b018e6accf
gkwydl <ha0402junglim@gmail.com>
2026-04-07 09:04:15 +0900

    TodoListApp HTML classNmae, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,25 +1,23 @@
+import "./todolist.css"
+
 function TodoListApp() {
   return (
     <>
-    <h1>ToDo ToDo</h1>
-    <form>
-      <input type="text"/>
-      <button>Add</button>
+    <div className="todo">
+    <h1 className="todo__title">ToDo ToDo</h1>
+    <form className="todo__form">
+      <input type="text" className="todo__input" placeholder="할 일을 입력하세요."/>
+      <button type="submit" className="todo__button todo__button--add">Add</button>
     </form>
-    <ul>
-      <li>
-        <input type="checkbox" />
-        <label htmlFor="">옷 싸기</label>
-        <button>🖋️</button>
-        <button>🗑️</button>
+    <ul className="todo__list">
+      <li className="todo__item todo__item--complete">
+        <input type="checkbox" className="todo__check" name="" id="chk-1"/>
+        <label htmlFor="chk-1"className="todo__label">옷 싸기</label>
+        <button className="todo__button todo__button--edit">🖋️</button>
+        <button className="todo__button todo__button--delete">🗑️</button>
         </li>
-      <li>
-        <input type="checkbox" />
-        <label htmlFor="">잠 자기</label>
-        <button>🖋️</button>
-        <button>🗑️</button>
-        </li>
-    </ul>
+      </ul>
+    </div>
     </>
   )
 }
